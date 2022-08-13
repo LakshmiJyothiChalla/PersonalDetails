80a9ed90056de04d205302acf50f589cb4d92a77
LakshmiJyothiChalla <lakshmijyothi.actimize@gmail.com>
2022-08-13 14:07:57 +0530

    Updated the code repository

---
+++ script.js
@@ -0,0 +1,85 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["relationship"] = document.getElementById("relationship").value;
+    formData["mobilenumber"] = document.getElementById("mobilenumber").value;
+	formData["address"] = document.getElementById("address").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.relationship;
+	cell3 = newRow.insertCell(3);
+    cell3.innerHTML = data.mobilenumber;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.address;
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("relationship").value = "";
+    document.getElementById("mobilenumber").value = "";
+	document.getElementById("address").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("relationship").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("mobilenumber").value = selectedRow.cells[3].innerHTML;
+	document.getElementById("address").value = selectedRow.cells[4].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.relationship;
+    selectedRow.cells[3].innerHTML = formData.mobilenumber;
+	selectedRow.cells[4].innerHTML = formData.address;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
